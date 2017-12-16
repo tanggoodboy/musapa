a86ef325e125367a7f894c44e7c1078b86aef54e
Junsung Lim <wns349@gmail.com>
2017-12-17 01:10:36 +0900

    Add log to show total users

--- src/main/java/wns/musapa/upbit/telegram/TelegramUsers.java
+++ src/main/java/wns/musapa/upbit/telegram/TelegramUsers.java
@@ -1,8 +1,12 @@
 package wns.musapa.upbit.telegram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TelegramUsers extends ConcurrentHashMap<Long, TelegramUser> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramUsers.class);
     private static TelegramUsers instance = null;
 
     private TelegramUsers() {
@@ -22,11 +26,17 @@ public class TelegramUsers extends ConcurrentHashMap<Long, TelegramUser> {
 
     public TelegramUser addUser(long id) {
         TelegramUser user = new TelegramUser(id);
-        this.put(id, user);
+        synchronized (this) {
+            this.put(id, user);
+            LOGGER.info("New user added. Total users: {}", this.size());
+        }
         return user;
     }
 
     public void removeUser(long id) {
-        this.remove(id);
+        synchronized (this) {
+            this.remove(id);
+            LOGGER.info("User removed Total users: {}", this.size());
+        }
     }
 }
