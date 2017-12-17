92bced0c2316d28603a8b2deea6486edb753959a
Junsung Lim <wns349@gmail.com>
2017-12-17 21:27:31 +0900

    Bug fix: print sorted csv

--- src/main/java/wns/musapa/util/downloader/UpbitHistoryDataDownloader.java
+++ src/main/java/wns/musapa/util/downloader/UpbitHistoryDataDownloader.java
@@ -16,11 +16,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 public class UpbitHistoryDataDownloader {
     private static final Logger LOGGER = LoggerFactory.getLogger(UpbitHistoryDataDownloader.class);
-    private static final String CODE = "CRIX.UPBIT.KRW-BTC";
     private static final String URL = "https://crix-api-endpoint.upbit.com/v1/crix/candles/minutes/1?code=%s&count=200&to=%sT%s.000Z";
 
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -37,7 +39,8 @@ public class UpbitHistoryDataDownloader {
         Date now = new Date();
 
         long timestamp = currentDate.getTime();
-        PrintWriter pw = new PrintWriter(new File("./data/" + code + "_" + simpleDateFormat.format(currentDate) + ".csv"));
+        String filename = String.format("./data/%s_%s_%s.csv", code, simpleDateFormat.format(currentDate), simpleDateFormat.format(now));
+        PrintWriter pw = new PrintWriter(new File(filename));
         pw.println("date,low,high,open,close,volume,weightedAverage");
         while (timestamp < now.getTime()) {
             currentDate = new Date(timestamp);
@@ -49,9 +52,22 @@ public class UpbitHistoryDataDownloader {
 
             JsonParser jp = new JsonParser();
             JsonArray root = jp.parse(new InputStreamReader(response.getEntity().getContent())).getAsJsonArray();
+            if (root.size() == 0) {
+                // No data available
+                timestamp += 24 * 60 * 60 * 1000; // Slide a day
+                continue;
+            }
+
+            // Sort
+            List<JsonObject> jsonList = new ArrayList<>();
             for (int i = 0; i < root.size(); i++) {
+                jsonList.add(root.get(i).getAsJsonObject());
+            }
+            jsonList.sort(Comparator.comparing(o -> o.get("candleDateTimeKst").getAsString()));
+
+            // Print
+            for (JsonObject jsonObject : jsonList) {
                 //convert
-                JsonObject jsonObject = root.get(i).getAsJsonObject();
                 String d = String.valueOf(sdfUpbit.parse(jsonObject.get("candleDateTimeKst").getAsString()).getTime());
                 pw.println(String.format("%s,%s,%s,%s,%s,%s,%s",
                         d,
@@ -71,7 +87,7 @@ public class UpbitHistoryDataDownloader {
     public static void main(String[] args) throws Exception {
 
         UpbitHistoryDataDownloader downloader = new UpbitHistoryDataDownloader();
-        downloader.run("CRIX.UPBIT.KRW-ETH", "2017-09-26"); // Sept 26 is the earliest possible.
+        downloader.run("CRIX.UPBIT.KRW-ADA", "2017-09-26"); // Sept 26 is the earliest possible.
         System.out.println("Done");
     }
 }
