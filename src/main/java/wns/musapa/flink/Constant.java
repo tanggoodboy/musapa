120a612ded4a0a58e90929d5c570657c84553d8b
Junsung Lim <wns349@gmail.com>
2018-01-22 21:14:56 +0900

    Add constant

---
+++ src/main/java/wns/musapa/flink/Constant.java
@@ -0,0 +1,6 @@
+package wns.musapa.flink;
+
+public class Constant {
+    public static final String MUSAPA_DEV_TELEGRAM_BOT_NAME = "";
+    public static final String MUSAPA_DEV_TELEGRAM_BOT_TOKEN = "";
+}
